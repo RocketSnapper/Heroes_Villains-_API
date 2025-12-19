0ef127cd3ce294618dc0b7357733f6e311b54cd4
usjamesmc <usjamesmc@gmail.com>
2025-12-18 18:23:46 -0600

    Updated settings, created supers and super_types apps and models.

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -34,6 +34,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'super_types',
+    'supers',
 ]
 
 MIDDLEWARE = [
@@ -105,3 +108,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from heroes_villains_project.local_settings import *
+except ImportError:
+    pass
---
+++ super_types/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class Super_Type(models.Model):
+    type = models.CharField(max_length=255)
